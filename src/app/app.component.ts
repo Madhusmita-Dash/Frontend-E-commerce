015f0ce7618f4de3efef34c77e23cc4f45adeabe
Biswajit Das <biswajit.ontheway@gmail.com>
2024-11-12 00:08:59 +0530

    user rolebased navbar and dashboard for admin and customer created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserStorageService } from './services/storage/user-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // corrected from `styleUrl` to `styleUrls`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DigitalE-commerceWeb';
+  
+  isCustomerLoggedIn: boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router: Router, private userStorageService: UserStorageService) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+      this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+    });
+  }
+
+  logout() {
+    UserStorageService.signOut();
+    this.router.navigateByUrl('login');
+  }
+  
 }
