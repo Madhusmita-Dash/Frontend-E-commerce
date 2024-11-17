93e995eb6419b4734e5a20b59596543310bd57ce
Satya <satyasarathi0014@gmail.com>
2024-11-17 16:06:04 +0530

    Showing Products to Admin & All Products

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts(): void {
+    this.products = [];
+    this.adminService.getAllProducts().subscribe(
+      (res) => {
+        res.forEach((element: any) => {
+          element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+          this.products.push(element);
+        });
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
 }
